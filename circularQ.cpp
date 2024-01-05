0b7ac767d200abd68ba0552d3276c49690a029cf
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-05 10:09:35 +0530

    enqueue dequeue in circular queue

---
+++ circularQ.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+
+using namespace std;
+class circularQueue{
+            private :
+                int front,rear,cap;
+                int* arr;
+            public :
+                circularQueue(int size){
+                    cap=size;
+                    front=-1;
+                    rear=-1;
+                    arr=new int[cap];
+                }
+            bool isEmpty(){
+                return(rear == -1 && front == -1);
+            }
+            bool isFull(){
+                return(rear+1)%cap==front;
+            }
+            void enqueue(int item){
+                if(isFull()){
+                    cout<<"the queue is full can't enqueue \n";
+                    return;
+                }
+                else if(isEmpty()){
+                    rear=front=0;
+                    arr[rear]=item;
+                }
+                else{
+                    arr[++rear]=item;
+                
+                }
+                 cout<<item<<" enqueued in queue \n";
+            }
+             int dequeue(){
+                if(isEmpty()){
+                    cout<<"the queue is empty can't dequeue \n";
+                    return -1;
+                }
+                else if(front==rear){
+                    int val=arr[front];
+                    front = rear = -1;
+                    return val;
+                }
+                else{
+                    int val=arr[front];
+                    front++;
+                    return val;
+                }
+            }
+};
+
+int main(){
+            int cap,item,choice;
+            cout<<"enter no of element in circular queue ";
+            cin>>cap;
+            circularQueue q1(cap);
+            do{
+                cout<<"1 enqueue element\n2 dequeue element\n0 exit\n";
+                cout<<"enter your operation ";
+                cin>>choice;
+                switch(choice){
+                    case 1:
+                        cout<<"enter the element you want to enqueue ";
+                        cin>>item;
+                        q1.enqueue(item);
+                        break;
+                    case 2:
+                        cout<<"the dequeued element is "<<q1.dequeue()<<endl;
+                        break;
+                    case 0:
+                        cout<<"exit from this code ";
+                        break;
+                    default :
+                        cout<<"invalid choice ";
+                        break;
+                }
+            }
+            while(choice);
+}
