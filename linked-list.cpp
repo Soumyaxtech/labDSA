f83ff7e6908021fd58fc48cc176f16018ae4cdc8
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-10 19:50:28 +0530

    insert at end

--- linked-list.cpp
+++ linked-list.cpp
@@ -18,6 +18,12 @@ void insertAtHead(Node* &head,int d){
             temp->next = head;
             head=temp;
 }
+void insertAtEnd(Node* &tail,int d){
+            // new node creation
+            Node*temp=new Node(d);
+            tail->next=temp;
+            tail=tail->next;
+}
 void print(Node* &head){
     Node*temp=head;
     while(temp!=NULL){
@@ -29,7 +35,9 @@ void print(Node* &head){
 int main(){
     Node*node1=new Node(10);
     Node*head=node1;
+    Node*tail=node1;
     insertAtHead(head,12);
-    insertAtHead(head,15);
+    insertAtEnd(tail,15);
+    insertAtEnd(tail,20);
     print(head);
 }
