27b95235211eb128c04f72fab39c5a5fe5777eb1
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-12 12:00:38 +0530

    delete in linkedlist

--- linked-list.cpp
+++ linked-list.cpp
@@ -24,16 +24,50 @@ void insertAtEnd(Node* &tail,int d){
             tail->next=temp;
             tail=tail->next;
 }
-void insertAtPosition(Node* &head,int pos,int d){
+void insertAtPosition(Node* &tail,Node* &head,int pos,int d){
+            if(pos==1){
+                insertAtHead(head,d);
+                return;
+            }
             Node*temp=head;
             int count=1;
             while(count<pos-1){
                 temp=temp->next;
                 count++;
             }
+            if(temp->next==NULL){
+                insertAtEnd(tail,d);
+                return;
+            }
             Node*node2=new Node(d);
             node2->next=temp->next;
             temp->next=node2;
+}
+void deleteHead(Node* &head){
+    Node*prev=NULL;
+    Node*curr=head;
+    prev=head;
+    head=curr->next;
+    delete prev;
+}
+void deleteAny(int pos,Node* &head){
+    if(pos==1){
+        deleteHead(head);
+        return;
+    }
+    else{
+        Node*curr=head;
+        Node*prev=NULL;
+        int count=1;
+        while(count<pos){
+            prev=curr;
+            curr=curr->next;
+            count++;
+        }
+        prev->next=curr->next;
+        curr->next=NULL;
+        delete curr;
+    }
 }
 void print(Node* &head){
     Node*temp=head;
@@ -56,6 +90,12 @@ int main(){
     insertAtEnd(tail,20);
     print(head);
      cout<<endl;
-    insertAtPosition(head,3,60);
+    insertAtPosition(tail,head,5,60);
+    print(head);
+    cout<<endl;
+    deleteHead(head);
+    print(head);
+    cout<<endl;
+    deleteAny(2,head);
     print(head);
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-Node*node1=new Node(10);
-    // Node*head=node1;
-=======
-else if(isEmpty()){
-            //     rear=front=0;
-            // }
->>>>>>> 0fc9245d07fa4e9644df2006e9172bef9294d3a3
+cout << "Current Linked List: ";
