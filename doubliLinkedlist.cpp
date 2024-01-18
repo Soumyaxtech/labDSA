5d10874974056f7581c2ca7c7acf7c53c442aa61
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-18 16:50:15 +0530

    insert at tail of a doubli linked list

--- doubliLinkedlist.cpp
+++ doubliLinkedlist.cpp
@@ -18,6 +18,12 @@ class Node{
         head->prev=temp;
         head=temp;
     }
+    void insertAtTail(Node* &tail,int d){
+        Node*temp=new Node(d);
+        tail->next=temp;
+        temp->prev=tail;
+        tail=tail->next;
+    }
      void print(Node* &head){
         Node*temp=head;
         while(temp){
@@ -39,11 +45,16 @@ class Node{
 int main(){
     Node*node1=new Node(10);
     Node*head=node1;
+    Node*tail=node1;
     print(head);
     cout<<length(head)<<endl;
     insertAtHead(head,56);
     print(head);
-     cout<<length(head)<<endl;
+    cout<<length(head)<<endl;
+    insertAtTail(tail,12);
+    print(head);
+    cout<<length(head)<<endl;
+
 
 
 }
