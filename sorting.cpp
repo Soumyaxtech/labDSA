6a99c7a85c1041b7378c0919a218e6b3b9d224d4
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-31 23:57:01 +0530

    add count in sorting

--- sorting.cpp
+++ sorting.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 void bubbleSort(int arr[],int n){
+    int count=0;
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                count++;
             }
         }
     }
+    cout<<count<<endl;
 }
 void ModifiedbubbleSort(int arr[],int n){
     bool swapped;
